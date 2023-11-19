e496930bcf24abd449b24355958c24850acdc31f
Victor <flyshoot954@gmail.com>
2023-11-19 04:33:18 +0400

    Добавьте файлы проекта.

---
+++ Lad№4.1_Prissyazhnyy/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целочисленный массив: ");
+int[] mas = new int[n];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = int.Parse(Console.ReadLine());
+}
+int difference = mas[1] - mas[0];
+for (int i = 2; i < mas.Length; i++)
+{
+    if (difference != mas[i] - mas[i - 1]) { Console.WriteLine("не образуют"); return; }
+}
+Console.WriteLine("Разность прогрессии: " + difference);
+
+
+
+
